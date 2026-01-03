49a1e736eaf736a4e65456609e3ea94a0e55ca6a
Github-Sonali <guptasonali.4545@gmail.com>
2026-01-03 16:43:38 +0530

    Fix API base URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://contact-management-app-gcbp.onrender.com",
+  baseURL: "https://contact-management-app-gcpb.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
